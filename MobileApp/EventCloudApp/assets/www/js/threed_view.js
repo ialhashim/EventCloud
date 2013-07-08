46dc8e40f55988953c09233afbb1f82968c6d6b5
ennetws <ennetws@gmail.com>
2013-07-08 01:43:06 -0700

    3D view works on Desktop and Android.

--- MobileApp/EventCloudApp/assets/www/js/threed_view.js
+++ MobileApp/EventCloudApp/assets/www/js/threed_view.js
@@ -1,12 +1,52 @@
-/* Global variables */
 
-eid = getParameterByName('eid');
-mid = getParameterByName('mid');
-userid = getParameterByName('uid');
-username = getUserName( userid );
+var camera, scene, renderer;
+var mesh;
+
 
 $(document).ready(function() { 
-	$("#debug").hide();
-
+	init( $('#viewportContent') );
+	animate();
 });
 
+function init( $parent ) {
+	renderer = new THREE.WebGLRenderer();
+	renderer.parent = $parent;
+	renderer.setSize( renderer.parent.width(), renderer.parent.height() );
+	
+	$parent.append( renderer.domElement );
+
+	camera = new THREE.PerspectiveCamera( 70, renderer.parent.width() / renderer.parent.height(), 1, 1000 );
+	camera.position.z = 400;
+
+	scene = new THREE.Scene();
+
+	var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+
+	var texture = THREE.ImageUtils.loadTexture( 'crate.gif' );
+	texture.anisotropy = renderer.getMaxAnisotropy();
+
+	var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+	camera.aspect = renderer.parent.width() / renderer.parent.height();
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( renderer.parent.width(), renderer.parent.height() );
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.005;
+	mesh.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+
+}
