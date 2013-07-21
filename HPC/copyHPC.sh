297997256e2ef95d402a4ee1d48eb1f502fed9e3
ibraheem <ennetws@gmail.com>
2013-07-21 16:07:38 -0700

    Added scripts for remote calls.

---
+++ HPC/copyHPC.sh
@@ -0,0 +1,2 @@
+s3cmd put FILE $1 s3://eventfulcloud-app 
+./sendHPC.sh "s3cmd get s3://eventfulcloud-app/$1"
---
+++ HPC/sendHPC.sh
@@ -0,0 +1,2 @@
+ssh ubuntu@ec2-54-244-111-115.us-west-2.compute.amazonaws.com -i hpc.pem $1
+ 
