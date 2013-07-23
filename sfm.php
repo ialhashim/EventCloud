fb2491f015c026d907111dfef5e42a21c161e89f
ennetws <ennetws@gmail.com>
2013-07-22 20:01:28 -0700

    Minor.

--- sfm.php
+++ sfm.php
@@ -1,21 +1,15 @@
 <?php
-    // Enable full error checking
-    ini_set('display_errors',1);
-    error_reporting(E_ALL);
-    $verbose = true;
-  
+	// Enable full error checking
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+	$verbose = true;
 	include_once('global.php');
 
-	echo "Preparing scripts.."; flush();
 	system("s3cmd get s3://eventfulcloud-app/HPC.tar.gz");
 	system("tar -xvf HPC.tar.gz");
-	system("cd HPC");
 	
 	$resolution = 300;
-	$reconCmd = "sh doJob.sh " . $eid . " " . $resolution;
+	$reconCmd = "cd HPC; sh doJob.sh " . $eid . " " . $resolution;
 	
-	echo "Executing: " . $reconCmd; flush();
-	system( $reconCmd );
-	
-	echo "Reconstruction done."; flush();
+	system( $reconCmd );	
 ?>
